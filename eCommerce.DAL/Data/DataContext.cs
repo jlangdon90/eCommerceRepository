c7eeee2121667afe777a0c208f6645706b1646f1
Jared Langdon <jlangdon0523@gmail.com>
2017-04-23 19:36:11 -0700

    Set up repositories

---
+++ eCommerce.DAL/Data/DataContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using eCommerce.Model;
+
+namespace eCommerce.DAL.Data
+{
+    class DataContext : DbContext
+    {
+        public DataContext() 
+            : base("DefaultConnection")
+        {
+            
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+
+    }
+}
